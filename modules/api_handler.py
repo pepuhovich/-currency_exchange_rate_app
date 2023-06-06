5146d5da34aa8f3d5cfea5cb9c786953e9027c73
pepuhovich <pepuhovich@gmail.com>
2023-06-06 14:56:00 +0200

    Create get_symbols function

--- modules/api_handler.py
+++ modules/api_handler.py
@@ -1,2 +1,20 @@
+import requests
+
 def request_api():
     ''
+
+def get_symbols():
+    symbols = []
+
+    # Contacting the API
+    response = requests.get('http://data.fixer.io/api/symbols?access_key=ac0abcdec1b89730c3adb9b70801d482')
+    # Loading the API response into the dictionary
+    load_response = response.json() 
+    symbols_dict = load_response['symbols']
+
+    #Parsing dict's keys into the list as case insestivie strings
+    symbols_keys = symbols_dict.keys()
+    for key in symbols_keys:
+        symbols.append(str.casefold(key))
+
+    return symbols
