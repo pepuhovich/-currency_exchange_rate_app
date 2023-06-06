08128d1f9a60b36903e72b127816a229b65bc39a
pepuhovich <pepuhovich@gmail.com>
2023-06-06 20:46:20 +0200

    Create request_latest function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from modules.api_handler import request_api, get_symbols
+from modules.api_handler import request_latest, get_all_symbols
 from modules.database_handler import show_history
 
-currency_symbols = get_symbols()
+currency_symbols = get_all_symbols()
 
 def verify_user_input(symbol):
     if symbol in currency_symbols:
@@ -13,18 +13,16 @@ def verify_user_input(symbol):
 
 while True:
     user_input = input('Enter currency pair of which you want to see the exchange rate (example: eur usd):')
-    first_currency = str.casefold(user_input[:3])
-    second_currency = str.casefold(user_input[4:])
-
-    print(first_currency)
-    print(second_currency)
+    first_currency = str.upper(user_input[:3])
+    second_currency = str.upper(user_input[4:])
 
     currency_1 = verify_user_input(first_currency)
     currency_2 = verify_user_input(second_currency)
 
     
     if currency_1 == True and currency_2 == True:
-        print('Correct pair')
+        convert_rate = request_latest(first_currency, second_currency)
+        print(f'1 {first_currency} = {convert_rate} {second_currency}')
         break
     elif currency_1 == True and currency_2 == False:
         print('Second currency symbol is incorrect')
--- modules/api_handler.py
+++ modules/api_handler.py
@@ -1,20 +1,35 @@
 import requests
 
-def request_api():
-    ''
+def request_latest(currency_1, currency_2):
+    # Contacting the API
+    response = requests.get(f'https://v6.exchangerate-api.com/v6/23c357f9e77fa0eb7d4495e4/pair/{currency_1}/{currency_2}')
+    # Loading the API response into the dictionary
+    load_response = response.json() 
+    rate = load_response['conversion_rate']
 
-def get_symbols():
+    return '%.2f' % rate
+
+
+def request_history(currency_1, currency_2):
+    # Contacting the API
+    response = requests.get(f'https://v6.exchangerate-api.com/v6/23c357f9e77fa0eb7d4495e4/pair/{currency_1}/{currency_2}')
+    # Loading the API response into the dictionary
+    load_response = response.json() 
+    rate = load_response['conversion_rate']
+
+    return '%.2f' % rate
+
+
+def get_all_symbols():
     symbols = []
 
     # Contacting the API
-    response = requests.get('http://data.fixer.io/api/symbols?access_key=ac0abcdec1b89730c3adb9b70801d482')
+    response = requests.get('https://v6.exchangerate-api.com/v6/23c357f9e77fa0eb7d4495e4/codes')
     # Loading the API response into the dictionary
     load_response = response.json() 
-    symbols_dict = load_response['symbols']
-
-    #Parsing dict's keys into the list as case insenstive strings
-    symbols_keys = symbols_dict.keys()
-    for key in symbols_keys:
-        symbols.append(str.casefold(key))
+    symbols_list = load_response['supported_codes']
+    # Parsing symbols into the list (skipping currency name)
+    for symbol in symbols_list:
+        symbols.append(symbol[0])
 
     return symbols
