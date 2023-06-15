b94d04fb549cc52a1445ec1f044e00c9f7cf58c4
pepuhovich <pepuhovich@gmail.com>
2023-06-15 09:41:06 +0200

    Add main method

--- main.py
+++ main.py
@@ -2,30 +2,30 @@ from modules.input import get_input, unpack_input, verify_input
 from modules.output import get_rate, print_output_data
 from modules.database_handler import send_to_db, print_from_db
 
+if __name__ == "__main__":
+    while True:
+        user_input = get_input()
 
-while True:
-    user_input = get_input()
-
-    if user_input == "quit":
-        print("...closing the app")
-        break
-    elif user_input == "history":
-        print_from_db()
-        break
-    else:
-        first_currency, second_currency = unpack_input(user_input)
-        if verify_input(first_currency, second_currency):
-            output_data = get_rate(first_currency, second_currency)
-            # Preparing data for printing
-            base_currency = output_data["base_curr"]
-            endpoint_currency = output_data["endpoint_curr"]
-            rate = output_data["rate"]
-            is_rate_higher = output_data["is_higher"]
-            # Print data
-            print_output_data(base_currency, endpoint_currency, rate, is_rate_higher)
-            # Preparing date for saving in database
-            datetime = output_data["date_time"]
-            # Save data in database
-            send_to_db(datetime, base_currency, endpoint_currency, rate, is_rate_higher)
+        if user_input == "quit":
+            print("...closing the app")
+            break
+        elif user_input == "history":
+            print_from_db()
+            break
         else:
-            print("wrong input")
+            first_currency, second_currency = unpack_input(user_input)
+            if verify_input(first_currency, second_currency):
+                output_data = get_rate(first_currency, second_currency)
+                # Preparing data for printing
+                base_currency = output_data["base_curr"]
+                endpoint_currency = output_data["endpoint_curr"]
+                rate = output_data["rate"]
+                is_rate_higher = output_data["is_higher"]
+                # Print data
+                print_output_data(base_currency, endpoint_currency, rate, is_rate_higher)
+                # Preparing date for saving in database
+                datetime = output_data["date_time"]
+                # Save data in database
+                send_to_db(datetime, base_currency, endpoint_currency, rate, is_rate_higher)
+            else:
+                print("wrong input")
